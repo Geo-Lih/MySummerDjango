7a03317d07ad5e9f662dac6acd6f7a912cd14e0b
Zhora <Lihitchenkozhorik>
2023-06-05 16:13:46 +0300

    linter fix

--- DaiDomivky/settings.py
+++ DaiDomivky/settings.py
@@ -135,6 +135,17 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',  # BASE_DIR is the root of your project
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'default_db_name'),
+        'USER': os.getenv('DB_USER', 'default_db_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'default_db_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
+
 AUTH_USER_MODEL = 'user.CustomUser'
 
 SESSION_COOKIE_AGE = 1200
